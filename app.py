b7ae609fd9486a437b8fa3ff0933e9b855a7e4e1
Cian O'Gralaigh <ihatecian@gmail.com>
2020-08-14 20:20:25 +0100

    Create new Date instance"

--- app.py
+++ app.py
@@ -4,8 +4,16 @@ from datetime import date
 """ Returns a datetime instance of today"""
 today = datetime.today()
 
-""" Returns todays date as a date object"""
+""" Returns todays date as a date object yyyy-mm-dd """
 todays_date = date.today()
 
-print(todays_date)
-print(type(todays_date))
+""" Can drill into date object"""
+todays_month = todays_date.month
+todays_day = todays_date.day
+todays_year = todays_date.year
+
+
+""" Create new instance of date"""
+christmas = date(2020, 12, 25)
+
+print(christmas)
