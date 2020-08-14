00ea4ea1a72460b87f1cd3bf0e1c868f2f34187c
Cian O'Gralaigh <ihatecian@gmail.com>
2020-08-14 20:17:06 +0100

    Add todays date"

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from datetime import datetime
 from datetime import date
 
+""" Returns a datetime instance of today"""
 today = datetime.today()
-print(today)
+
+""" Returns todays date as a date object"""
+todays_date = date.today()
+
+print(todays_date)
+print(type(todays_date))
